f7eee6a93e1ec5f286c52aa7d6165a11de58c35c
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-15 20:56:24 +0300

    working

---
+++ Example_002/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+Clear();
+Write("input number:");
+int num1 = Convert.ToInt32(ReadLine());
+int num2 = int.Parse(ReadLine()!);
+int sqr2 = Convert.ToInt32(Math.Pow(num2,2));
+if(num1 == sqr2){
+WriteLine($"sqr of number {num2} = {sqr2} Yes!!!!");
+}
+else{
+WriteLine($"sqr of number {num2} != {sqr2} NO!!!!");
+}
