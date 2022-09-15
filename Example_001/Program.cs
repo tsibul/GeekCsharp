9dc5b1f7ca23b0b17a846271ff41a263b0751390
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-15 20:44:09 +0300

    using static System.Console;

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -1,8 +1,10 @@
-﻿Console.Clear();
-Console.Write("input number:");
-int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = int.Parse(Console.ReadLine()!);
+﻿using static System.Console;
+
+Clear();
+Write("input number:");
+int num1 = Convert.ToInt32(ReadLine());
+int num2 = int.Parse(ReadLine()!);
 int sqr1 = num1*num1;
 int sqr2 = Convert.ToInt32(Math.Pow(num2,2));
-Console.WriteLine($"sqr of number {num1} = {sqr1}");
-Console.WriteLine($"sqr of number {num2} = {sqr2}");
+WriteLine($"sqr of number {num1} = {sqr1}");
+WriteLine($"sqr of number {num2} = {sqr2}");
