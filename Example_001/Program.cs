3566fd0304d230bfc5d2c82bce141dca4a3a9374
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-15 20:31:45 +0300

    sqr of number

---
+++ Example_001/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.Write("input number")
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine()!);
+int sqr1 = num1*num1;
+int sqr2 = Convert.ToInt32(Math.Pow(num2,2));
+Console.WriteLine($"sqr of number{num1} = {sqr1}");
+Console.WriteLine($"sqr of number{num2} = {sqr2}");
