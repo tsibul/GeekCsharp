e2f8edb57c0e663cd676963b89288fbb68d20af4
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-16 19:58:28 +0300

    some spaces for interface

--- HomeWork_1_160922/Program.cs
+++ HomeWork_1_160922/Program.cs
@@ -2,22 +2,22 @@
 Clear();
 
 // input 3 numbers for all tasks
-Write("input number 1:");
+Write("input number 1: ");
 int num1 = Convert.ToInt32(ReadLine());
-Write("input number 2:");
+Write("input number 2: ");
 int num2 = Convert.ToInt32(ReadLine());
-Write("input number 3:");
+Write("input number 3: ");
 int num3 = Convert.ToInt32(ReadLine());
 
 //Задача 2: Напишите программу, которая на вход принимает 
 //два числа и выдаёт, какое число большее, а какое меньшее.
 //use num1 and num2
 
-string comparison = $"first number ({num1}) more than second number ({num2})";
+string comparison = $"first number ( {num1} ) more than second number ( {num2} )";
 if(num1 < num2){
-    comparison = $"first number ({num1}) less than second number ({num2})";
+    comparison = $"first number ( {num1} ) less than second number ( {num2} )";
 }
 else if(num1==num2){
-    comparison = $"both nambers {num1} are equal";
+    comparison = $"both nambers ( {num1} ) are equal";
 }
 Write(comparison);
