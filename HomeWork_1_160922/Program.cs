6cad851c6ff2830015994a3533358320379e8024
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-16 19:45:04 +0300

    initial commit

---
+++ HomeWork_1_160922/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+
+Clear();
+Write("input number 1:");
+int num1 = Convert.ToInt32(ReadLine());
+Write("input number 2:");
+int num2 = Convert.ToInt32(ReadLine());
+Write($"{num1} - {num2}");
