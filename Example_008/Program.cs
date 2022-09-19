fdfc7fd8bb97bbe4449a01c8fade39fc9648b0a2
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-19 23:04:37 +0300

    Task15

--- Example_008/Program.cs
+++ Example_008/Program.cs
@@ -1,11 +1,11 @@
 ﻿using static System.Console;
 Clear();
 
-int number = new Random().Next(100, 999+1);
+int number = new Random().Next(100, 999 + 1);
 WriteLine($"number = {number}");
-int n0 = number%10;
-int n2 = number/100;
-int n1 = (number - n2*100)/10;
+int n0 = number % 10;
+int n2 = number / 100;
+int n1 = (number - n2 * 100) / 10;
 WriteLine($"{n0}{n1}{n2}");
 //WriteLine($"result = {n0*100+n1*10+n2}");
 
@@ -13,7 +13,8 @@ Write("input number 2:");
 string num2 = ReadLine();
 int n = num2.Length;
 int num2 = int.Parse(num2);
-int i=0;
-while(i<n){
-    
+int i = 0;
+while (i < n)
+{
+
 }
--- Homework_2_190922/Program.cs
+++ Homework_2_190922/Program.cs
@@ -1,10 +1,26 @@
 ﻿using static System.Console;
 Clear();
 
-// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
-// и на выходе показывает вторую цифру этого числа.
+WriteLine("Задача 10: Напишите программу, которая принимает на вход трёхзначное число");
+WriteLine("и на выходе показывает вторую цифру этого числа.");
+WriteLine("");
 
 int number = new Random().Next(100, 999+1);
 WriteLine($"number = {number}");
 int result = number/10%10;
 WriteLine($"second digit = {result}");
+
+WriteLine("");
+WriteLine("Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день"); 
+WriteLine("недели, и проверяет, является ли этот день выходным.");
+WriteLine("");
+
+Write("input weekday No: ");
+int weekday = int.Parse(ReadLine()!);
+string message = weekday==7||weekday==6? "Yes" : weekday>0&&weekday<6? "No": $"{weekday} not a weekday number!";
+WriteLine(message);
+
+WriteLine("");
+WriteLine("Задача 13: Напишите программу, которая выводит третью цифру"); 
+WriteLine("заданного числа или сообщает, что третьей цифры нет.");
+WriteLine(""); 
