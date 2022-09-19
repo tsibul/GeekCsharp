e9bd7130b57b6bfbca839800e191a9a9ffa94b43
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-19 22:04:20 +0300

    initial commit

---
+++ Example_008/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+Clear();
+
+int number = new Random().Next(100, 999+1);
+WriteLine($"number = {number}");
+int n0 = number%10;
+int n2 = number/100;
+int n1 = (number - n2*100)/10;
+WriteLine($"{n0}{n1}{n2}");
+//WriteLine($"result = {n0*100+n1*10+n2}");
+
+Write("input number 2:");
+string num2 = ReadLine();
+int n = num2.Length;
+int num2 = int.Parse(num2);
+int i=0;
+while(i<n){
+    
+}
