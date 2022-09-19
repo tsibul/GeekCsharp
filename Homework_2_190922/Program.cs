3d02126f21a3f8cf8b547cf0bd88f86ecb20b5e4
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-19 22:43:33 +0300

    Task 10

--- Homework_2_190922/Program.cs
+++ Homework_2_190922/Program.cs
@@ -1,2 +1,10 @@
 ﻿using static System.Console;
 Clear();
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+int number = new Random().Next(100, 999+1);
+WriteLine($"number = {number}");
+int result = number/10%10;
+WriteLine($"second digit = {result}");
