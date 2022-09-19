b965fbb0d66e31ba85bff4c915a3d7ee446487c9
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-19 20:53:29 +0300

    Working

--- Example_005/Program.cs
+++ Example_005/Program.cs
@@ -1,1 +1,9 @@
 ﻿using static System.Console;
+Clear();
+
+int number = new Random().Next(100, 999+1);
+WriteLine($"number = {number}");
+int n0 = number%10;
+int n2 = number/100;
+WriteLine($"{n2}{n0}");
+WriteLine($"result = {n2*10+n0}");
---
+++ Example_006/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+Clear();
+Write("input number 1:");
+int num1 = Convert.ToInt32(ReadLine());
+Write("input number 2:");
+int num2 = Convert.ToInt32(ReadLine());
+WriteLine($"{num1} - {num2}");
+if(num2%num1 == 0){
+    WriteLine($"{num2} кратно {num1}");
+}
+else{
+    WriteLine($"остаток от деления {num2} на {num1} = {num2%num1}");
+}
