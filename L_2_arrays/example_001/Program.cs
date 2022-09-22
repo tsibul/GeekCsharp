b73aec17801b148ca757e9e747dde6ed3d5c4979
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-22 20:36:20 +0300

    looking for quarter

--- L_2_arrays/example_001/Program.cs
+++ L_2_arrays/example_001/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using static System.Console;
+Clear();
+Write("input x :");
+int x = Convert.ToInt32(ReadLine()!);
+Write("input y :");
+int y = Convert.ToInt32(ReadLine()!);
+
+
+if (x == 0 || y == 0)
+{
+    WriteLine(" на линии координат");
+    return;
+}
+if (x > 0 && y > 0)
+{
+    WriteLine("1");
+    return;
+};
+if (x < 0 && y > 0)
+{
+    WriteLine("2");
+    return;
+};
+if (x < 0 && y < 0)
+{
+    WriteLine("3");
+    return;
+};
+if (x > 0 && y < 0)
+{
+    WriteLine("4");
+    return;
+};
