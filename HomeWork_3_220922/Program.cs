7a24ffeae52549c2a15d706a9e9b85094cbd9075
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-23 00:12:07 +0300

    Task19

---
+++ HomeWork_3_220922/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+Clear();
+
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Write("input 5 digits number :");
+int num5 = Convert.ToInt32(ReadLine());
+if(num5 >= 100000 || num5 < 10000){
+    WriteLine($"{num5} is not 5 digits number");
+    return;
+}
+int i = 0;
+int num5_reverse = 0;
+int num5_ = num5;
+string result = "No";
+while (i < 5){
+    i++;
+    num5_reverse = num5_reverse*10 + num5_%10;
+    num5_ = num5_/10;
+}
+if(num5 == num5_reverse){
+    result = "Yes";
+}
+WriteLine(result);
--- L_2_arrays/example_002/Program.cs
+++ L_2_arrays/example_002/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+// Write("input number :");
+// int number = Convert.ToInt32(ReadLine());
+
+// int i = 0;
+// string result = "";
+// while(i < number){
+//     i++;
+//     int sq_r = i*i;
+//     result += Convert.ToString(sq_r) + ", ";
+// }
+// WriteLine(result);
+
+
+Write("input day :");
+int day = Convert.ToInt32(ReadLine());
+string result = "";
+
+
+// switch(day){
+//     case 1:
+//         result = "Monday";
+//         break;
+//     case 2: 
+//         result = "Tuesday";
+//         break;    
+//     case 3: 
+//         result = "Wednesday";
+//         break;    
+//     case 4: 
+//         result = "Thursday";
+//         break;    
+//     case 5: 
+//         result = "Friday";
+//         break;    
+//     case 6: 
+//         result = "Saturday";
+//         break;    
+//     case 7: 
+//         result = "Sunday";
+//         break;
+//     default:
+//         result = "Not a weekday";
+//         break;        
+//     } 
+// WriteLine(result);
+
+
+string [] weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+try {WriteLine(weekdays[day-1]);}
+catch{WriteLine("Not a weekday");} 
+
+
+//Два стрелка стреляют по банкам, первый - слева-направо, второй - справа - налево
+//в какой-то момент они простреливают одну банку, каждый знает сколько он прострелил,
+//вычислить кто сколько не поразил.
+
+// Write("input shooter 1 :");
+// int sht1 = Convert.ToInt32(ReadLine());
+// Write("input shooter 2 :");
+// int sht2 = Convert.ToInt32(ReadLine());
+// WriteLine($"shooter 1 didn't shoot {sht2 - 1}");
+// WriteLine($"shooter 2 didn't shoot {sht1 - 1}");
+
+//3 человека делают кораблики  дев в 2р быстрее чем маль, вычислить сколько сделал каждый
+
+WriteLine("");
+Write("input ships :");
+int ships = Convert.ToInt32(ReadLine());
+if(ships%6 != 0){
+    WriteLine("incorrect input");
+    return;    
+}
+WriteLine($"girl - {ships/3*2}, boy - {ships/6}"); 
+
