12b1675144904aa312be02b7507ab30a9f4779ee
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-22 20:54:31 +0300

    distance between points

--- L_2_arrays/example_001/Program.cs
+++ L_2_arrays/example_001/Program.cs
@@ -1,33 +1,48 @@
 ﻿using static System.Console;
 Clear();
-Write("input x :");
-int x = Convert.ToInt32(ReadLine()!);
-Write("input y :");
-int y = Convert.ToInt32(ReadLine()!);
+// Write("input x :");
+// int x = Convert.ToInt32(ReadLine()!);
+// Write("input y :");
+// int y = Convert.ToInt32(ReadLine()!);
 
 
-if (x == 0 || y == 0)
-{
-    WriteLine(" на линии координат");
-    return;
-}
-if (x > 0 && y > 0)
-{
-    WriteLine("1");
-    return;
-};
-if (x < 0 && y > 0)
-{
-    WriteLine("2");
-    return;
-};
-if (x < 0 && y < 0)
-{
-    WriteLine("3");
-    return;
-};
-if (x > 0 && y < 0)
-{
-    WriteLine("4");
-    return;
-};
+// if (x == 0 || y == 0)
+// {
+//     WriteLine(" на линии координат");
+//     return;
+// }
+// if (x > 0 && y > 0)
+// {
+//     WriteLine("1");
+//     return;
+// };
+// if (x < 0 && y > 0)
+// {
+//     WriteLine("2");
+//     return;
+// };
+// if (x < 0 && y < 0)
+// {
+//     WriteLine("3");
+//     return;
+// };
+// if (x > 0 && y < 0)
+// {
+//     WriteLine("4");
+//     return;
+// };
+
+
+WriteLine("first point");
+Write("input x1 :");
+double x1 = Convert.ToDouble(ReadLine()!);
+Write("input y1 :");
+double y1 = Convert.ToDouble(ReadLine()!);
+WriteLine("");
+WriteLine("second point");
+Write("input x2 :");
+double x2 = Convert.ToDouble(ReadLine()!);
+Write("input y2 :");
+double y2 = Convert.ToDouble(ReadLine()!);
+double result = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+WriteLine($"distance between ({x1},{y1}) and ({x2},{y2}) = {result}");
