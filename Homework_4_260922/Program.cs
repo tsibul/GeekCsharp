864b87f9a95c286848b7895da30bc2d6e076e27c
Ilya Tsibul <tsigel@vikivostok.ru>
2022-09-26 23:49:29 +0300

    task25

--- Homework_4_260922/Program.cs
+++ Homework_4_260922/Program.cs
@@ -1,13 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write ("input A : ");
+int A = Convert.ToInt32(ReadLine()!);
+Write ("input B : ");
+int B = Convert.ToInt32(ReadLine()!);
+WriteLine($"{A}^{B}={GetNaturalPower(A, B)}");
 
 
-//Methodsgit add
+//Methods
 
 // Решение через отдельные методы
 // Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B с использованием цикла.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+int GetNaturalPower(int num1, int num2){
+    if(num2 <= 0){
+        return -1;
+    } 
+    int power = 1;
+    int i = 1;
+    while(i <= num2){
+        power *= num1;
+        i++;
+    }
+    return power;
+}
 
 // Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
