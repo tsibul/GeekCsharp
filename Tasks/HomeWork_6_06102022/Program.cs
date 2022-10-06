1e0ba9ff61e646b8c9a9a55c35c7accae79c4edb
Ilya Tsibul <tsigel@vikivostok.ru>
2022-10-06 23:39:17 +0300

    Task41

--- Tasks/HomeWork_6_06102022/Program.cs
+++ Tasks/HomeWork_6_06102022/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using GeekLib;
+Clear();
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+CountPositive();
+
+void CountPositive()
+{
+int count = 0;
+bool operate = true;
+while (operate){ }
+    Write("Input number if you want to continue or anykey to stop: ");
+    string input = (ReadLine());
+    if (int.TryParse(input, number){
+        if (number > 0) count++;
+    }
+    else operate = false;
+    WriteLine($"Positive numbers = {count}");
+while (ReadKey().Key==ConsoleKey.Enter);
+}
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int count = 0;
+do
+{
+    Write("Input number: ");
+    if (int.Parse(ReadLine()!) > 0) count++;
+    WriteLine($"Positive numbers = {count}");
+    WriteLine("Continue? Yes - enter, No - any key");
+} while (ReadKey().Key == ConsoleKey.Enter);
