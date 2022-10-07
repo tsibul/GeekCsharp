c18ebc24d4e772c0021f839e0ca3087e0df6d78b
Ilya Tsibul <tsigel@vikivostok.ru>
2022-10-07 08:32:31 +0300

    HW 7

--- Tasks/HomeWork_7_07102022/Program.cs
+++ Tasks/HomeWork_7_07102022/Program.cs
@@ -9,10 +9,10 @@ Clear();
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-// Write("Input rows columns minimum and maximum divided by space: ");
-// int [] inps = MyMethods.GetArrayFromString(ReadLine()!);
-// double[,] array = MyMethods.GetDoubleMatrixArray(inps[0], inps[1], inps[2], inps[3]);
-// MyMethods.PrintMatrix(array);
+Write("Input rows columns minimum and maximum divided by space: ");
+int [] inps = MyMethods.GetArrayFromString(ReadLine()!);
+double[,] array = MyMethods.GetDoubleMatrixArray(inps[0], inps[1], inps[2], inps[3]);
+MyMethods.PrintMatrix(array);
 
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
@@ -23,16 +23,16 @@ Clear();
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-// Write("Input rows columns minimum and maximum divided by space: ");
-// int [] inps = MyMethods.GetArrayFromString(ReadLine()!);
-// int [,] arr = MyMethods.GetMatrixArray(inps[0], inps[1], inps[2], inps[3]);
-// MyMethods.PrintMatrix(arr);
-// Write("Input row and column  divaded by space : ");
-// inps = MyMethods.GetArrayFromString(ReadLine()!);
-// if (MyMethods.IfIndexInArray(arr, inps[0], inps[1]))
-// {
-//     WriteLine($"--> {arr[inps[0], inps[1]]}");
-// } else WriteLine("--> no such element");
+Write("Input rows columns minimum and maximum divided by space: ");
+int [] inps = MyMethods.GetArrayFromString(ReadLine()!);
+int [,] arr = MyMethods.GetMatrixArray(inps[0], inps[1], inps[2], inps[3]);
+MyMethods.PrintMatrix(arr);
+Write("Input row and column  divaded by space : ");
+inps = MyMethods.GetArrayFromString(ReadLine()!);
+if (MyMethods.IfIndexInArray(arr, inps[0], inps[1]))
+{
+    WriteLine($"--> {arr[inps[0], inps[1]]}");
+} else WriteLine("--> no such element");
 
 
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
