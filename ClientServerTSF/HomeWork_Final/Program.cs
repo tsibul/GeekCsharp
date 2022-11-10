1cb725ff94e1c5b8a55853d7c1197747e1069976
Ilya Tsibul <tsigel@vikivostok.ru>
2022-11-10 13:55:02 +0300

    initial commit

---
+++ ClientServerTSF/HomeWork_Final/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+Clear();
+
+Write("input maximum element Length: ");
+int elLength = Convert.ToInt32(ReadLine()!); 
+Write("input array Length: ");
+int arLength = Convert.ToInt32(ReadLine()!); 
+
+string [] startArray = CreateRandomStringArray(elLength, arLength);
+Write($"input array [{String.Join(", ", startArray)}]");
+
+
+string [] CreateRandomStringArray(int elementLength, int arrayLength)
+{
+    string [] stringArray = new string [arrayLength];
+    for (int i = 0; i < arrayLength; i++)
+    {
+       int tmpLength = new Random().Next(1, elementLength + 1);
+       string tmpElement = string.Empty;
+       for (int j = 0; j < tmpLength; j++)
+        {
+            int tmpUnicode = new Random().Next(25, 75);
+            char tmpCharacter = (char) tmpUnicode;
+            string tmpChar = tmpCharacter.ToString();
+            tmpElement += tmpChar;
+        }
+        stringArray[i] = tmpElement;
+    }
+    return stringArray;
+}
---
+++ Tasks/BootCamp/Program.cs
@@ -0,0 +1,123 @@
+﻿using static System.Console;
+using GeekLib;
+Clear();
+
+//Дана последовательность из N целых чисел и число K. Необходимо сдвинуть всю последовательность 
+//(сдвиг - циклический) на |K| элементов вправо, если K – положительное и влево, если отрицательное.
+
+//Первая строка входного файла INPUT.TXT содержит натуральное число N, 
+//во второй строке записаны N целых чисел Ai, а в последней – целое число K. (1 ≤ N ≤ 105, |K| ≤ 105, |Ai| ≤ 100).
+
+//В выходной файл OUTPUT.TXT выведите полученную последовательность.
+
+// 5
+// 5 3 7 4 6
+// 3
+
+// 7 4 6 5 3
+
+// 5
+// 5 3 7 4 6
+// -3
+
+// 4 6 5 3 7
+
+
+//Сортировка выбором
+Console.WriteLine("Введите кол-во элементов массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+// Заполнение массива
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+// Cортировка
+for (int i = 0; i < n - 1; i++)
+{
+    int MinIndex = i;
+    for (int j = i + 1; j < n; j++)
+    {
+        if (array[j] < array[MinIndex])
+            MinIndex = j; 
+    }
+    int temp;
+    temp = array[MinIndex];
+    array[MinIndex] = array[i];
+    array[i] = temp;
+}
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+
+/*
+Cортировка пузырьком
+Начальный массив: [3, 1, 5, 0, 7, 9, 8]
+*/
+
+Console.WriteLine("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите значения массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+    Console.WriteLine(i + "[" + string.Join(", ", array) + "]");
+}
+
+
+//Быстрая сортировка
+
+/*
+1. arr = {1, 0, -6, 2, 5, 3, 2} 
+2. pivot = arr[6]  (опорный элемент)
+3. Вызвать шаги 1-2 для подмассива слева от pivot и справа от pivot
+*/
+int[] arr = { 0, -5, 2, 3, 5, 9, -1, 7 };
+QuickSort(arr, 0, arr.Length - 1);
+Console.Write($"Отсортированный массив {string.Join(", ", arr)}");
+
+static void QuickSort(int[] inputArray, int minIndex, int maxIndex)
+{
+    if (minIndex >= maxIndex) return;
+    int pivot = GetPivotIndex(inputArray, minIndex, maxIndex);
+    QuickSort(inputArray, minIndex, pivot - 1);
+    QuickSort(inputArray, pivot + 1, maxIndex);
+    return;
+}
+static int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
+{
+    int pivotIndex = minIndex - 1;
+    for (int i = minIndex; i <= maxIndex; i++)
+    {
+        if (inputArray[i] < inputArray[maxIndex])
+        {
+            pivotIndex++;
+            Swap(inputArray, i, pivotIndex);
+        }
+    }
+    pivotIndex++;
+    Swap(inputArray, pivotIndex, maxIndex);
+    return pivotIndex;
+}
+static void Swap(int[] inputArray, int leftValue, int rightValue)
+{
+    int temp = inputArray[leftValue];
+    inputArray[leftValue] = inputArray[rightValue];
+    inputArray[rightValue] = temp;
+}
