dc43dbab697ea677e19234b0f99502fe88f5f885
Ilya Tsibul <tsigel@vikivostok.ru>
2022-11-10 14:20:05 +0300

    create cut array method

--- ClientServerTSF/HomeWork_Final/Program.cs
+++ ClientServerTSF/HomeWork_Final/Program.cs
@@ -9,17 +9,45 @@ int arLength = Convert.ToInt32(ReadLine()!);
 string [] startArray = CreateRandomStringArray(elLength, arLength);
 Write($"input array [{String.Join(", ", startArray)}]");
 
+WriteLine();
+Write("Как резать будем ? ");
+int cutLength = Convert.ToInt32(ReadLine()!); 
 
+string [] newArray = CutArray(startArray, cutLength);
+Write($"input array [{String.Join(", ", newArray)}]");
+
+string [] CutArray(string [] inputArray, int cutParametr)
+{
+    var j = 0;
+    for (int i = 0; i < inputArray.Length; i++)    
+    {
+        if(inputArray[i].Length <= cutParametr) j++;
+    }
+    string [] outArray = new string [j];
+
+    var j = 0;
+    for (int i = 0; i < inputArray.Length; i++)    
+    {
+        if(inputArray[i].Length <= cutParametr)
+        {
+            outArray[j] = inputArray[i];
+            j++;
+        }
+    }
+    return outArray;
+}
 
 string [] CreateRandomStringArray(int elementLength, int arrayLength)
 {
     string [] stringArray = new string [arrayLength];
     for (int i = 0; i < arrayLength; i++)
     {
+//genegate random length of element for forLoop        
        int tmpLength = new Random().Next(1, elementLength + 1);
        string tmpElement = string.Empty;
        for (int j = 0; j < tmpLength; j++)
         {
+//generate random number and use as unicode for random simbol            
             int tmpUnicode = new Random().Next(20, 120);
             char tmpCharacter = (char) tmpUnicode;
             string tmpChar = tmpCharacter.ToString();
